becf4270e74b7832bc22643bac68387f6d0be906
Dmitry Astapov <dastapov@gmail.com>
2010-05-24 22:21:11 +0100

    Fix greediness in IMPORTS
    Ignore-this: dbf3a6c05ef8f12ac340bba98c1bd299
    
    darcs-hash:a84770cb6228bfa982c806a4894c3889cefbfef1

--- Language/ASN1/Parser.hs
+++ Language/ASN1/Parser.hs
@@ -172,7 +172,7 @@ exports =
 
 imports = 
   do { reserved "IMPORTS"  
-     ; (many1 symbolsFromModule) `endBy` semi
+     ; (many1 $ try symbolsFromModule) `endBy` semi
      }
      <?> "Imports"
 
