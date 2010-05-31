ad4e926640274e82849481467d931b5f4b121394
Dmitry Astapov <dastapov@gmail.com>
2010-05-31 19:07:37 +0100

    Fix: disallow whitespace between closing ' and B (or H) in bstring and hstring
    Ignore-this: 617b5b550503dfcc07235e0105cb7254
    
    darcs-hash:d26e923e554d0e668bf6b331aa2587c3420e79d1

--- Language/ASN1/Parser.hs
+++ Language/ASN1/Parser.hs
@@ -143,7 +143,7 @@ type HString = BinString
 hstring = binString " 0123456789ABCDEF" 'H' <?> "hstring"
 
 data BinString = BinString Char String deriving (Eq,Ord,Show, Typeable, Data)
-binString allowedSet marker = BinString marker <$> ( ( symbol "'" *> (filter (not.isSpace) <$> many (oneOf allowedSet) ) ) <* symbol "'" <* char marker <* whiteSpace)
+binString allowedSet marker = BinString marker <$> ( ( symbol "'" *> (filter (not.isSpace) <$> many (oneOf allowedSet) ) ) <* char '\'' <* char marker <* whiteSpace)
 
 newtype CString = CString String deriving (Eq,Ord,Show, Typeable, Data)
 -- TODO: need to fix double "" inside cstring?
